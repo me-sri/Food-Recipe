ab52c049abba01c33b5725ef549c56d3f4d6177a
sriram <109786782+me-sri@users.noreply.github.com>
2022-09-07 11:35:56 +0530

    Add files via upload

---
+++ Food Recipe/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Component/Style.css';
+import Meal from './Component/Meal';
+function App() {
+  return(
+<Meal>
+
+</Meal>
+
+  )
+  
+}
+
+export default App;
